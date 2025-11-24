0d5c3b3c4a3d5a4a15d78e6d0e46bc7dddb03047
kimwonu <25_13@bssm.hs.kr>
2025-11-24 20:13:18 +0900

    api 없이 구현 완료

---
+++ static/js/login.js
@@ -0,0 +1,7 @@
+function openModal() {
+  document.getElementById("welcomeModal").classList.add("active");
+}
+
+function closeModal() {
+  document.getElementById("welcomeModal").classList.remove("active");
+}
---
+++ static/js/signup.js
@@ -0,0 +1,6 @@
+function openModal() {
+  document.getElementById("warningModal").classList.add("active");
+}
+function closeModal() {
+  document.getElementById("warningModal").classList.remove("active");
+}
